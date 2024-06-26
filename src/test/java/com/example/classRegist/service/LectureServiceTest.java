eac4f3815786830508ae2095e6c4ee0942e512d8
JiheeOh <jenny2596@naver.com>
2024-06-26 23:30:02 +0900

    :test_tube: lectureSeriveTest 테스트 코드 추가

---
+++ src/test/java/com/example/classRegist/service/LectureServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.classRegist.service;
+
+import com.example.classRegist.application.service.LectureService;
+import com.example.classRegist.common.exception.MemberNotFoundException;
+import com.example.classRegist.domain.dto.RequestDto;
+import com.example.classRegist.infra.repository.ApplyRepository;
+import com.example.classRegist.infra.repository.LectureRepository;
+import com.example.classRegist.infra.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LectureServiceTest {
+
+    @InjectMocks
+    LectureService lectureService;
+
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    LectureRepository lectureRepo;
+
+    @Mock
+    ApplyRepository applyRepo;
+
+    @DisplayName("예외 1: 등록되지 않은 사용자일 경우 : MemberNotFoundException ")
+    @Test
+    void memberNotFoundException_whenApply() {
+        // given
+        String memberId = "testId";
+        String letureId = "lecture";
+        RequestDto requestDto = new RequestDto(memberId,letureId);
+
+        // when : LectureService의 checkMember() return null로 설정
+        when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
+
+        // Then : 미등록 사용자 신청 시도 시 MemberNotFoundException
+        assertThrows(MemberNotFoundException.class, ()->lectureService.applyLecture(requestDto));
+
+    }
+
+
+
+
+
+
+
+}
