7bc9600b12a0f2a2137d4c1bceb855f9e9829d93
JiheeOh <jenny2596@naver.com>
2024-06-27 00:26:06 +0900

    :sparkles: DB 비관적 락을 사용한 동시성 처리 기능 완료
    - DB 비관적 락을 사용해서 특강 수용인원을 넘지 않도록 기능 개발 완료

--- src/main/java/com/example/classRegist/application/service/LectureService.java
+++ src/main/java/com/example/classRegist/application/service/LectureService.java
@@ -1,22 +1,24 @@
 package com.example.classRegist.application.service;
 
-import com.example.classRegist.domain.entity.Apply;
-import com.example.classRegist.domain.entity.ApplyPk;
-import com.example.classRegist.domain.entity.Lecture;
-import com.example.classRegist.domain.dto.LectureDTO;
-import com.example.classRegist.domain.dto.RequestDto;
 import com.example.classRegist.common.exception.DupliApplyException;
 import com.example.classRegist.common.exception.LectureNotFoundException;
 import com.example.classRegist.common.exception.MemberNotFoundException;
 import com.example.classRegist.common.exception.RegiFailException;
+import com.example.classRegist.domain.dto.LectureDTO;
+import com.example.classRegist.presenter.BaseRequest;
+import com.example.classRegist.domain.entity.Apply;
+import com.example.classRegist.domain.entity.ApplyPk;
+import com.example.classRegist.domain.entity.Lecture;
 import com.example.classRegist.infra.repository.ApplyRepository;
 import com.example.classRegist.infra.repository.LectureRepository;
 import com.example.classRegist.infra.repository.MemberRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LectureService {
@@ -26,6 +28,7 @@ public class LectureService {
     private final LectureRepository lectureRepo;
 
     private final ApplyRepository applyRepo;
+
 
     public LectureService(MemberRepository memberRepo, LectureRepository lectureRepo, ApplyRepository applyRepo) {
         this.memberRepo = memberRepo;
@@ -38,28 +41,25 @@ public class LectureService {
      *
      * @param request : 사용자 Id, 강의 Id
      */
-    public void applyLecture(RequestDto request) {
-
+    public void applyLecture(BaseRequest request) {
         String memberId = request.getMemberId();
         String lectureId = request.getLectureId();
 
         // 등록할 수 있는 유효한 사용자/강의인지 확인
         checkValidate(memberId, lectureId);
-        Lecture lecture = getAvailableLecture(lectureId);
 
-        // 등록 진행
-        Apply apply = new Apply(new ApplyPk(memberId, lectureId));
-        applyRepo.save(apply);
-
-        // 잔여인원 감소시키기
-        reduceLeftOver(lecture);
+        // 등록 시도
+        tryApply(memberId,lectureId);
     }
+
+
 
 
     /**
      * 수업을 등록할 수 있는 사용자인지 확인
      * - 미등록 사용자 ID일 경우 : MemberNotFoundException
      * - 중복 수업 신청 시 : DupliApplyException
+     *
      * @param memberId  : 찾으려는 사용자 ID
      * @param lectureId : 등록하려는 강의 ID
      * @return Member : 등록된 사용자
@@ -71,6 +71,32 @@ public class LectureService {
         }
         return true;
     }
+
+    /**
+     * 수업 등록 메소드
+     * 배타적 락을 사용하여
+     * 1. 등록할 수 있는 lecture인지 확인
+     * 2. 등록 테이블에 데이터 적재
+     * 3. 잔여인원 감소시키기
+     *  위 3가지 step을 한 transaction에서 관리
+     *
+     * @param memberId : 사용자 Id
+     * @param lectureId : 강의 Id
+     */
+    @Transactional
+    protected void tryApply(String memberId, String lectureId) {
+
+        // step1 : 등록할 수 있는 Lecture인지 확인
+        Lecture lecture = getAvailableLecture(lectureId);
+
+        // step2 : 등록 진행
+        Apply apply = new Apply(new ApplyPk(memberId, lectureId));
+        applyRepo.save(apply);
+
+        // step3 : 잔여인원 감소시키기
+        reduceLeftOver(lecture);
+    }
+
 
     /**
      * 등록할 수 있는 강의만 return
@@ -110,7 +136,7 @@ public class LectureService {
      * @return 특강 신청 완료 여부
      */
     public boolean isApplied(String memberId, String lectureId) {
-        return applyRepo.existsById(new ApplyPk(memberId,lectureId));
+        return applyRepo.existsById(new ApplyPk(memberId, lectureId));
     }
 
     /**
